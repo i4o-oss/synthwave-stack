113c26d8452643714fcd7a885d04e74288159ec4
Ilango <rajagopalilango@gmail.com>
2023-02-20 18:03:57 +0530

    👌 IMPROVE: Remove components

--- common/components/buttons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './button'
-export { default as DangerButton } from './danger'
-export { default as PrimaryButton } from './primary'
-export { default as IconButton } from './icon-button'
