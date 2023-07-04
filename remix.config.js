ef83845371dd39fd701541752f663e33f25f7af7
Ilango <rajagopalilango@gmail.com>
2023-07-04 17:32:40 +0530

    👌 IMPROVE: Change deploy target and use some latest remix feature flags

--- remix.config.js
+++ remix.config.js
@@ -2,11 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-	serverBuildTarget: 'vercel',
-	// When running locally in development mode, we use the built in remix
-	// server. This does not understand the vercel lambda module format,
-	// so we default back to the standard build output.
-	server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+	postcss: true,
+	tailwind: true,
+	future: {
+		v2_routeConvention: true,
+	},
 	ignoredRouteFiles: ['**/.*'],
 	// appDirectory: "app",
 	// assetsBuildDirectory: "public/build",
