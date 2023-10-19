4e6a7df5a2d1ac8af1ad9c2173a33073fa909eb0
Ilango <rajagopalilango@gmail.com>
2023-10-19 17:50:20 +0530

    👌 IMPROVE: Update remix config

--- remix.config.js
+++ remix.config.js
@@ -4,14 +4,6 @@
 module.exports = {
 	postcss: true,
 	tailwind: true,
-	future: {
-		v2_dev: true,
-		v2_errorBoundary: true,
-		v2_headers: true,
-		v2_meta: true,
-		v2_normalizeFormMethod: true,
-		v2_routeConvention: true,
-	},
 	ignoredRouteFiles: ['**/.*'],
 	serverModuleFormat: 'cjs',
 	// appDirectory: "app",
